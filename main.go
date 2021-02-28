08e8778fa5e875e40d2870e7d575113dba1aaae6
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2021-02-28 21:48:50 +0300

    #1 Initial attempt to get username by AWS profile implicitly

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/iam"
+)
+
+func main() {
+	username, err := getUserName()
+	if err != nil {
+		fmt.Println("Unable to determine username implicitly by used AWS profile")
+		return
+	}
+	fmt.Println(username)
+}
+
+func getUserName() (string, error) {
+	svc := iam.New(session.New())
+	result, err := svc.GetUser(&iam.GetUserInput{})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case iam.ErrCodeNoSuchEntityException:
+				fmt.Println(iam.ErrCodeNoSuchEntityException, aerr.Error())
+			case iam.ErrCodeServiceFailureException:
+				fmt.Println(iam.ErrCodeServiceFailureException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return "", err
+	}
+
+	return aws.StringValue(result.User.UserName), nil
+}
